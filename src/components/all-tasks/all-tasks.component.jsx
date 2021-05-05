9d637dd6c4815cb24a7b37d5e22d012269d14e99
MatteoCinti <matteocinti@hotmail.com>
2021-05-05 09:20:25 +0200

    adding Redux

--- src/components/all-tasks/all-tasks.component.jsx
+++ src/components/all-tasks/all-tasks.component.jsx
@@ -1,26 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
 // Import files and CSS
 import './all-tasks.styles.scss';
+import { setActiveList } from "../../redux/todos/todos.actions"
 
 // Import components
 import Button from "../button/button.component"
 import Input from "../input/input.component"
 
-const AllTasks = () => (
-   <div className="all-tasks">
-      <h2 className="task-list-title">My Lists</h2>
-      
-      <ul className="task-list">
-         <li id="active-list" className="list-name">Youtube</li>
-         <li className="list-name">Work</li>
-         <li className="list-name">Groceries</li>
-      </ul>
-   
+const AllTasks = (props) => {
 
-      <form action="">
-         <Input type="text" className="new-input list" placeholder="New List Name" />
-         <Button className="list create" aria-label="create new list">+</Button>
-      </form>
-   </div>
-)
+   return(
+      <div className="all-tasks">
+        
+         <h2 className="task-list-title">My Lists</h2>
+         
+         <ul className="task-list">
+         
+            {props.todos.map((list, index) =>{
+               const id = list.isActive ? 'active-list' : null ;
+               return <li id={id} className="list-name" key={index} onClick={() => props.setActiveList(index)}>{list.listName}</li>
+            })}
 
-export default AllTasks;
+         </ul>
+
+         <form action="">
+            <Input type="text" className="new-input list" placeholder="New List Name" />
+            <Button className="list create" aria-label="create new list">+</Button>
+         </form>
+      </div>
+)}
+
+const mapStateToProps = state => ({
+   todos: state.todos
+})
+
+
+const mapDispatchToProps = dispatch => { 
+   return {
+      setActiveList: payload => dispatch(setActiveList(payload)) 
+   }
+} 
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllTasks);
---
+++ src/redux/todos/todos.actions.js
@@ -0,0 +1,9 @@
+export const setNewList = payload => ({
+   type: 'SET_NEW_LIST',
+   payload: payload
+ });
+
+export const setActiveList = payload => ({
+   type: 'SET_ACTIVE_LIST',
+   payload: payload
+ });
