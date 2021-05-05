d203d24716e897ff65becf4d2ad89af17694eaa0
MatteoCinti <matteocinti@hotmail.com>
2021-05-05 12:19:01 +0200

    Revert "adding Redux"
    This reverts commit 9d637dd6c4815cb24a7b37d5e22d012269d14e99.

--- src/redux/root-reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import todosReducer from './todos/todos.reducer';
-
-export default combineReducers({
-  todos: todosReducer
-});
--- src/redux/todos/todos.actions.js
+++
@@ -1,9 +0,0 @@
-export const setNewList = payload => ({
-   type: 'SET_NEW_LIST',
-   payload: payload
- });
-
-export const setActiveList = payload => ({
-   type: 'SET_ACTIVE_LIST',
-   payload: payload
- });
